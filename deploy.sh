cfee7e13d4b606114a18cfbee98b4ee71d347d80
qiwihui <qwh005007@gmail.com>
2019-01-25 11:05:49 +0800

    deploy with keep commit history

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Initialize target with currently deployed files
+git clone --depth 1 --branch=gh-pages https://github.com/qiwihui/blog.git .deploy_git
+
+cd .deploy_git
+
+# Remove all files before they get copied from ../public/
+# so git can track files that were removed in the last commit
+find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
+
+cd ../
+
+# Run deployment
+- hexo clean
+- hexo generate --deploy --quiet
